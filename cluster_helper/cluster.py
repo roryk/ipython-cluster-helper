75c950828ef6011848688e25889f7b7ba2f71ee7
Rory Kirchner <rory.kirchner@gmail.com>
2013-03-21 13:19:00 -0300

    Fixed broken link.

--- cluster_helper/cluster.py
+++ cluster_helper/cluster.py
@@ -4,7 +4,7 @@ Uses IPython parallel to setup a cluster and manage execution:
 
 http://ipython.org/ipython-doc/stable/parallel/index.html
 Borrowed from Brad Chapman's implementation:
-https://github.com/chapmanb/bcbio-nextgen/blob/master/bcbio/pipeline/ipython.py
+https://github.com/chapmanb/bcbio-nextgen/blob/master/bcbio/distributed/ipython.py
 """
 import contextlib
 import os
