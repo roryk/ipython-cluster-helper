3323e512a0183320b7c6c4ae4c0a3aa541680ac1
roryk <rory.kirchner@gmail.com>
2013-04-29 16:12:51 -0400

    Increment version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name = "ipython-cluster-helper",
-      version = "0.1.3",
+      version = "0.1.4",
       author = "Rory Kirchner",
       author_email = "rory.kirchner@gmail.com",
       description = "Simplify IPython cluster start up and use for multiple schedulers.",
