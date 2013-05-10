7ead448a41863007d96a170fb298eba40771f88a
chapmanb <chapmanb@50mail.com>
2013-05-10 09:10:57 -0400

    Push new release with ipython fixes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name = "ipython-cluster-helper",
-      version = "0.1.5a",
+      version = "0.1.5",
       author = "Rory Kirchner",
       author_email = "rory.kirchner@gmail.com",
       description = "Simplify IPython cluster start up and use for multiple schedulers.",
