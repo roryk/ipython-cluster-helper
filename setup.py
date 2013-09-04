c8c6fea574044a662022f79faa2e3c8d5f024835
Brad Chapman <chapmanb@50mail.com>
2013-09-04 04:24:13 -0500

    Release v0.1.13: include memory fixes for PBS and Torque

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name = "ipython-cluster-helper",
-      version = "0.1.12",
+      version = "0.1.13",
       author = "Rory Kirchner",
       author_email = "rory.kirchner@gmail.com",
       description = "Simplify IPython cluster start up and use for multiple schedulers.",
