f183b30bd3f3fca4e8f7108c6947194041938b12
Luca Beltrame <luca.beltrame@marionegri.it>
2013-09-19 12:12:51 +0200

    Use locate_profile() from IPython.utils.path instead of calling ipython locate from subprocess.

--- cluster_helper/cluster.py
+++ cluster_helper/cluster.py
@@ -20,8 +20,10 @@ import sys
 from IPython.parallel import Client
 from IPython.parallel.apps import launcher
 from IPython.parallel import error as iperror
+from IPython.utils.path import locate_profile
 from IPython.utils import traitlets
 from IPython.utils.traitlets import (List, Unicode, CRegExp)
+
 
 # ## Custom launchers
 
@@ -566,19 +568,11 @@ def create_throwaway_profile():
     subprocess.check_call(cmd, shell=True)
     return profile
 
-
-def get_ipython_dir(profile):
-    proc = subprocess.Popen("ipython locate", stdout=subprocess.PIPE, shell=True)
-    ipython_dir = proc.stdout.read().strip()
-    profile_dir = "profile_{0}".format(profile)
-    return os.path.join(ipython_dir, profile_dir)
-
-
 def get_url_file(profile, cluster_id):
     if os.path.isdir(profile) and os.path.isabs(profile):
         ipython_dir = profile
     else:
-        ipython_dir = get_ipython_dir(profile)
+        ipython_dir = locate_profile(profile)
     security_dir = os.path.join(ipython_dir, "security")
     url_file = "ipcontroller-{0}-client.json".format(cluster_id)
     return os.path.join(security_dir, url_file)
