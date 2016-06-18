e6ef315bfd2fe9f69bfd603505f381ba044d041b
Alain Péteut <alain.peteut@yahoo.com>
2016-06-18 16:32:55 +0200

    use pip to install

--- setup.py
+++ setup.py
@@ -6,14 +6,16 @@ from pip.download import PipSession
 install_reqs = parse_requirements("requirements.txt", session=PipSession())
 reqs = [str(ir.req) for ir in install_reqs]
 
-setup(name = "ipython-cluster-helper",
-      version = "0.5.2",
-      author = "Rory Kirchner",
-      author_email = "rory.kirchner@gmail.com",
-      description = "Simplify IPython cluster start up and use for multiple schedulers.",
+
+setup(name="ipython-cluster-helper",
+      version="0.5.2",
+      author="Rory Kirchner",
+      author_email="rory.kirchner@gmail.com",
+      description="Simplify IPython cluster start up and use for "
+      "multiple schedulers.",
       long_description=(open('README.rst').read()),
-      license = "MIT",
+      license="MIT",
       zip_safe=False,
-      url = "https://github.com/roryk/ipython-cluster-helper",
-      packages = find_packages(),
-      install_requires = reqs)
+      url="https://github.com/roryk/ipython-cluster-helper",
+      packages=find_packages(),
+	  install_requires=reqs)
